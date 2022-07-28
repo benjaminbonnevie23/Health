8566fd136f56931fa5a822496977dfeb648609d5
Benjamin Joseph M. Bonnevie <bonneviebenjaminjoseph01@gmail.com>
2022-07-28 18:37:48 +0800

    FINAL_2.0

---
+++ Health/Views/FirstViewPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Health.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstViewPage : ContentPage
+    {
+        public FirstViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AboutPage());
+        }
+    }
+}
